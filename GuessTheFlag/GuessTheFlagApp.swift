34b1c3913a244b3d9604ba13e993e1d5864ed460
Scott Obara <scott@rubbscreations.com>
2021-01-04 13:33:12 +1100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Scott Obara on 4/1/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
